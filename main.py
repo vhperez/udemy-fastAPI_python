bcd6951ab2fd6d8cc0f36443fc33290750d182b8
Hugo Perez <vhperez@outlook.com>
2025-01-02 10:01:36 -0600

    cambio de puerto

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
 from fastapi import FastAPI
-from fastapi.responses import HTMLResponse, JSONResponse
-from pydantic import BaseModel
-
+from fastapi.responses import HTMLResponse
 from bd.database import engine, Base
 from routers.movie import routerMovie
 from routers.user import routerUser
+import uvicorn
+import os
 
 app = FastAPI(
     title='Mi primer fast api con python',
@@ -23,3 +23,8 @@ Base.metadata.create_all(bind=engine)
 def read_root(): #funcion que se manda llamar
     #return {'hello':'world'} #regresa la respuesta
     return HTMLResponse('<h2>Hola desde fast api</h2>')
+
+
+if(__name__=='__main__'):
+    port = int(os.environ.get('PORT',8000))
+    uvicorn.run('main:app',host='0.0.0.0',port=port)
